d4e2940db54326a69e949032fd8321de0c485365
camain-elie <camain.elie@gmail.com>
2021-03-23 15:55:29 +0100

    Completed default, error, disabled, helperText and icons inputs user stories. Fixed a bug on the menu display.

--- src/components/Input.js
+++ src/components/Input.js
@@ -8,16 +8,25 @@ const Input = ({value, helperText, placeHolderText, hover, focus, startIcon, end
         ${size === 'sm' ? 'input--sm' : (size === 'md' ? 'input--md' : (size === 'lg' ? 'input--lg' : ''))}
         ${fullWidth ? 'input--fullWidth' : ''}
         ${startIcon ? 'input--start-icon' : ''}
+        ${endIcon ? 'input--end-icon' : ''}
         ${disabled ? 'input--disabled' : ''}
         ${error ? 'input--error' : ''}
     `
 
-    return (<p> let's start again !</p>
+    return (
+        <div className={classList}>
+            <label >{label}</label>
+                {startIcon && <i className="material-icons startIcon">{startIcon}</i>}
+                <input className="" placeholder={placeHolderText} />
+                {endIcon && <i className="material-icons endIcon">{endIcon}</i>}
+            
+            {helperText && <p className="helperText">{helperText}</p>}
+        </div>
     )
 }
 
 Input.defaultProps = {
-
+    placeHolderText: 'Placeholder',
 }
 
 export default Input
